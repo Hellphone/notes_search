70385294ba8d76d23ed011ff58272364b0c8e199
Nikita <inoy2005@inbox.ru>
2019-03-24 16:41:54 +0300

    Some refactoring

--- src/script.js
+++ src/script.js
@@ -1,51 +1,66 @@
+var records = {
+	"Title search": "Title search title search title search", 
+	"JavaScript is great!": "JavaScript is great! Really!", 
+	"Google Analytics": "You might not need Google Analytics. Really.", 
+	"Learn JavaScript": "It's cool, I must admit", 
+	"Some text": "Some text some text some text some text some text",
+	"Another text": "Another text another text another text another text"
+}
+
 function init() {
 	var searchForm = document.getElementById('search-form');
 	var searchQueryInput = document.getElementById('search-query');
-	searchForm.onkeyup = getSearchResult;
+	var resultBlock = document.querySelector('#result ul');
+
+	searchQueryInput.onkeyup = addOnKeyUpHandler(searchQueryInput, resultBlock);
 	searchForm.onsubmit = submitForm;
 	searchQueryInput.focus();
+	resultBlock.innerHTML = '';
 }
 
-function getSearchResult() {
-	var records = {
-		"Title search": "Title search title search title search", 
-		"JavaScript is great!": "JavaScript is great! Really!", 
-		"Google Analytics": "You might not need Google Analytics. Really.", 
-		"Learn JavaScript": "It's cool, I must admit", 
-		"Some text": "Some text some text some text some text some text",
-		"Another text": "Another text another text another text another text"
-	}
+function addOnKeyUpHandler(elem, resultBlock) {
+	elem.addEventListener('keyup', function(e) {
+		var query = elem.value;
+		var arResult = getSearchResult(query);
+		resultBlock.innerHTML = '';
+		showResult(arResult, resultBlock);
+	});
+}
 
-	var query = document.getElementById('search-query').value;
-
-	var resultBlock = document.querySelector('#result ul');
-	resultBlock.innerHTML = '';
-
+function getSearchResult(query) {
 	if (query != '') {
 		var arResult = {};
 
 		for (var key in records) {
 			if (key.toLowerCase().indexOf(query.toLowerCase()) >= 0
-				|| records[key].toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+			|| records[key].toLowerCase().indexOf(query.toLowerCase()) >= 0) {
 				arResult[key] = records[key];
 			}
 		}
 
-		for (var key in arResult) {
+	} else {
+		return {};
+	}
+
+	return arResult;
+}
+
+function showResult(source, target) {
+	if (Object.keys(source).length === 0) {
+		target.innerHTML = 'No match';
+	} else {
+		for (var key in source) {
 			var newEl = document.createElement('li');
 			var newUl = document.createElement('ul')
 			var newEl2 = document.createElement('li');
 			newEl.innerHTML = key;
-			resultBlock.appendChild(newEl);
-			newEl2.innerHTML = arResult[key];
+			target.appendChild(newEl);
+			newEl2.innerHTML = source[key];
 			newEl.appendChild(newUl);
 			newUl.appendChild(newEl2);
 		}
-
-		if (arResult.length == 0) {
-			resultBlock.innerHTML = 'No match';
-		}
 	}
+	
 }
 
 function submitForm(e, query) {
